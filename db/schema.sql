a17fd4d509caabdbae43e688680cff89fef7037e
T. GREEN <toddgreen@utexas.edu>
2016-07-29 00:02:04 -0500

    160728_Initial Files

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(200) NOT NULL,
+  devoured BOOLEAN NOT NULL,
+  timestamp TIME,
+  PRIMARY KEY(id)
+);
